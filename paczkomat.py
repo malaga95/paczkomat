214d66b7c4aa8875d0ac0650ab52feb04d2a1461
Oskar <ookroj95@gmail.com>
2021-04-13 07:24:29 +0200

    Zarys zadania

---
+++ paczkomat.py
@@ -0,0 +1,10 @@
+#Setting values
+packages_amount = input("Hello, thank You for choosing us to deliver\n How many packages would You like to send ? \n")
+packages_amount = int(packages_amount)
+#Value to incrementate
+counter = 0
+
+for {counter} in range {packages_amount}
+    package_weight = input("Podaj ile waży paczka\n ")
+    if package_weight == 0
+        
