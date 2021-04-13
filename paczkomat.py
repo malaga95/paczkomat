3d30869f60dc80f5c3f4fe18c6ee4bb221d553c5
Oskar <ookroj95@gmail.com>
2021-04-13 07:57:12 +0200

    szkic zadania problem z komenda break w lini 7

--- paczkomat.py
+++ paczkomat.py
@@ -1,10 +1,21 @@
 #Setting values
 packages_amount = input("Hello, thank You for choosing us to deliver\n How many packages would You like to send ? \n")
 packages_amount = int(packages_amount)
+#Getting properly package amount
+if packages_amount <= 0:
+   print("Sorry, you gave incorrect value")
+   break
+   if packages_amount >= 99:
+      print("Slow down a little bit, maximum amount of packages that you can add at once is 99.")
+      break
+else:
+   continue
 #Value to incrementate
 counter = 0
 
-for {counter} in range {packages_amount}
-    package_weight = input("Podaj ile waży paczka\n ")
-    if package_weight == 0
-        
+for counter in range (packages_amount):
+    print("czesc")
+
+    #package_weight = input("Podaj ile waży paczka\n ")
+    
+    #if package_weight == 0
